1f4a048367995ae620b19d0ff6d9f2fe5d1ec9af
pjhsk113 <pjhsk113@naver.com>
2021-07-10 19:05:30 +0900

    [feat] User Repository 생성

---
+++ src/main/java/java/io/realworld/backend/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package java.io.realworld.backend.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.realworld.backend.domain.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
