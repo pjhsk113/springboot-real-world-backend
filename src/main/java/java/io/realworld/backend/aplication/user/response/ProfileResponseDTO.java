ad40ad24cb00a2b3b5fed07f0004ec2cb7e6fc9f
pjhsk113 <pjhsk113@naver.com>
2021-07-11 13:58:01 +0900

    [feat] response용 DTO 생성
    - api 스펙에 맞는 응답형태

---
+++ src/main/java/java/io/realworld/backend/aplication/user/response/ProfileResponseDTO.java
@@ -0,0 +1,29 @@
+package java.io.realworld.backend.aplication.user.response;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Getter;
+
+import java.io.realworld.backend.domain.user.Profile;
+
+@Getter
+@JsonRootName("profile")
+public class ProfileResponseDTO {
+    private final String username;
+    private final String bio;
+    private final String image;
+    private final boolean following;
+
+    private ProfileResponseDTO(String username, String bio, String image, boolean following) {
+        this.username = username;
+        this.bio = bio;
+        this.image = image;
+        this.following = following;
+    }
+
+    public static ProfileResponseDTO of(Profile profile) {
+        return new ProfileResponseDTO(profile.getUsername(),
+                profile.getBio(),
+                profile.getImage(),
+                profile.isFollowing());
+    }
+}
---
+++ src/main/java/java/io/realworld/backend/aplication/user/response/UserResponseDTO.java
@@ -0,0 +1,38 @@
+package java.io.realworld.backend.aplication.user.response;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Getter;
+
+import java.io.realworld.backend.domain.user.User;
+
+@Getter
+@JsonRootName("user")
+public class UserResponseDTO {
+    private final String email;
+    private final String token;
+    private final String username;
+    private final String bio;
+    private final String image;
+
+    private UserResponseDTO(String email, String token, String username, String bio, String image) {
+        this.email = email;
+        this.token = token;
+        this.username = username;
+        this.bio = bio;
+        this.image = image;
+    }
+
+    public static UserResponseDTO of(User user) {
+        return new UserResponseDTO(user.getEmail(), "",
+                user.getProfile().getUsername(),
+                user.getProfile().getBio(),
+                user.getProfile().getImage());
+    }
+
+    public static UserResponseDTO of(User user, String token) {
+        return new UserResponseDTO(user.getEmail(), token,
+                user.getProfile().getUsername(),
+                user.getProfile().getBio(),
+                user.getProfile().getImage());
+    }
+}
