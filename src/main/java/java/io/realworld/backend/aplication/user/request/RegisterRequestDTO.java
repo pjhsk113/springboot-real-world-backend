e018073b898f60ef7ca502dc13f66394e36ecccc
pjhsk113 <pjhsk113@naver.com>
2021-07-15 19:28:38 +0900

    [feat] getter 추가

--- src/main/java/java/io/realworld/backend/aplication/user/request/RegisterRequestDTO.java
+++ src/main/java/java/io/realworld/backend/aplication/user/request/RegisterRequestDTO.java
@@ -1,12 +1,14 @@
 package java.io.realworld.backend.aplication.user.request;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.io.realworld.backend.domain.user.UserRegisterModel;
 
 @JsonRootName("user")
+@Getter
 public class RegisterRequestDTO {
     @NotBlank
     private String username;
@@ -17,7 +19,7 @@ public class RegisterRequestDTO {
     @NotBlank
     private String password;
 
-    UserRegisterModel toRegisterRequest() {
+    public UserRegisterModel toRegisterRequest() {
         return new UserRegisterModel(
                 this.username,
                 this.email,
