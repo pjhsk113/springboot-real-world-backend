d9cc2d15fd63803dcefa557a9b3d524c389e0a91
JanghoPark <pjhsk113@naver.com>
2021-07-25 15:36:05 +0900

    [feat] lombok 모듈 추가

--- src/main/java/study/io/realworld/backend/aplication/user/request/LoginRequestDTO.java
+++ src/main/java/study/io/realworld/backend/aplication/user/request/LoginRequestDTO.java
@@ -1,12 +1,18 @@
 package study.io.realworld.backend.aplication.user.request;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import study.io.realworld.backend.domain.user.UserLoginModel;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @JsonRootName("user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class LoginRequestDTO {
     @Email
     private String email;
